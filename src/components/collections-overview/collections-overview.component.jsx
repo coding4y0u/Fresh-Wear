0138dd375f4cbb6733538bd820844905bbd66940
Daniyyel Yosef Bey <coding4you.net@gmail.com>
2019-09-12 13:52:43 -0400

    removing redux logger from production build

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -6,7 +6,7 @@ import './collections-overview.styles.scss';
 import CollectionPreview from './../collection-preview/collection-preview.component';
 
 const CollectionsOverview = ({ collections }) => (
-  <div className="collections-ovew">
+  <div className="collections-overview">
     {collections.map(({id, ...otherCollectionProps}) => (
       <CollectionPreview key={id} {...otherCollectionProps} />
     ))}
